28eb3e631317315f7450e892001d1e6c59d6da82
kasunvimarshana <kasunvmail@gmail.com>
2020-06-16 22:19:05 +0530

    read_write_excel

---
+++ src/test/java/excel_data/WriteExcelFile.java
@@ -0,0 +1,58 @@
+package excel_data;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcelFile {
+
+	public void writeExcel(String filePath, String fileName, String sheetName, String[] dataToWrite) throws IOException{
+        //Create an object of File class to open xlsx file
+        File file = new File(filePath + "\\" + fileName);
+        //Create an object of FileInputStream class to read excel file
+        FileInputStream inputStream = new FileInputStream(file);
+        Workbook workBookObject = null;
+        //Find the file extension by splitting  file name in substring and getting only extension name
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+        //Check condition if the file is xlsx file
+        if(fileExtensionName.equals(".xlsx")){
+        	//If it is xlsx file then create object of XSSFWorkbook class
+        	workBookObject = new XSSFWorkbook(inputStream);
+        }
+        //Check condition if the file is xls file
+        else if(fileExtensionName.equals(".xls")){
+            //If it is xls file then create object of XSSFWorkbook class
+        	workBookObject = new HSSFWorkbook(inputStream);
+        }    
+        //Read excel sheet by sheet name    
+        Sheet sheetObject = workBookObject.getSheet(sheetName);
+        //Get the current count of rows in excel file
+        int rowCount = (sheetObject.getLastRowNum() - sheetObject.getFirstRowNum());
+        //Get the first row from the sheet
+        Row rowObject = sheetObject.getRow(0);
+        //Create a new row and append it at last of sheet
+        Row newRowObject = sheetObject.createRow(rowCount+1);
+        //Create a loop over the cell of newly created Row
+        for(int j = 0; j < rowObject.getLastCellNum(); j++){
+        	//Fill data in row
+        	Cell cellObject = newRowObject.createCell(j);
+        	cellObject.setCellValue(dataToWrite[j]);
+        }
+        //Close input stream
+        inputStream.close();
+        //Create an object of FileOutputStream class to create write data in excel file
+        FileOutputStream outputStream = new FileOutputStream(file);
+        //write data in the excel file
+        workBookObject.write(outputStream);
+        //close output stream
+        outputStream.close();
+	}
+	
+}
